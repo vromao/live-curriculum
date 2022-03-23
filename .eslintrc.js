d549dd2d582e5547c273d7713f61e8b187543ec8
vromao <viniciusromao.contato@gmail.com>
2022-03-22 23:23:59 -0300

    feat: add storybook initial version

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     __TRAILING_SLASH__: true,
     __BASE_PATH__: true,
   },
-  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:storybook/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
